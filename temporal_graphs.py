0ce914734e3dffb7b315f4245c7ea04b9e69e0bb
Christian <chrisaug@rogers.com>
2021-11-17 21:18:08 -0500

    added code for part D

--- temporal_graphs.py
+++ temporal_graphs.py
@@ -1,8 +1,12 @@
+import itertools
 import matplotlib.pyplot as plt
 from os import read
 import networkx as nx
+from networkx.algorithms.community.centrality import girvan_newman
 import json
 import os
+
+from networkx.generators import community
 
 path = f"{os.getcwd()}\\tmp_dblp_coauthorship.json"
 
@@ -74,53 +78,53 @@ print(report)
 
 ### temporal b) ###
 
-dblp_2006 = nx.Graph()
-print("Generating graph dblp_2006 ...")
-for t in data:
-    n1, n2, year = t
-    if (year == 2006):
-        dblp_2006.add_edge(n1, n2)
+# dblp_2006 = nx.Graph()
+# print("Generating graph dblp_2006 ...")
+# for t in data:
+#     n1, n2, year = t
+#     if (year == 2006):
+#         dblp_2006.add_edge(n1, n2)
 
-gcc = get_gcc(dblp_2006)
-report = report_nodes_edges(gcc)
-print(report)
+# gcc = get_gcc(dblp_2006)
+# report = report_nodes_edges(gcc)
+# print(report)
 
 ### temporal c) ###
 
-dblp_2005_w = nx.Graph()
-print("Generating graph dblp_2005_w ...")
-for t in data:
-    n1, n2, year = t
-    if (year == 2005):
-        if (dblp_2005_w.has_edge(n1, n2)):
-            dblp_2005_w[n1][n2]["weight"] += 1
-        else:
-            dblp_2005_w.add_edge(n1, n2, weight=1)
+# dblp_2005_w = nx.Graph()
+# print("Generating graph dblp_2005_w ...")
+# for t in data:
+#     n1, n2, year = t
+#     if (year == 2005):
+#         if (dblp_2005_w.has_edge(n1, n2)):
+#             dblp_2005_w[n1][n2]["weight"] += 1
+#         else:
+#             dblp_2005_w.add_edge(n1, n2, weight=1)
 
-gcc = get_gcc(dblp_2005_w)
-report = report_nodes_edges(gcc)
-print(report)
+# gcc = get_gcc(dblp_2005_w)
+# report = report_nodes_edges(gcc)
+# print(report)
 
 ### Node and Edge Importance in Graphs i) ###
-print("Getting Page rank importance ...")
-pr_dblp_2005 = get_first_N(get_sorted_pagerank(dblp_2005), 50)
-pr_dblp_2006 = get_first_N(get_sorted_pagerank(dblp_2006), 50)
-pr_dblp_2005_w = get_first_N(get_sorted_pagerank(dblp_2005_w), 50)
-print("Writing results ...")
-write_json("pr_dblp_2005.json", pr_dblp_2005)
-write_json("pr_dblp_2006.json", pr_dblp_2006)
-write_json("pr_dblp_2005_w.json", pr_dblp_2005_w)
+# print("Getting Page rank importance ...")
+# pr_dblp_2005 = get_first_N(get_sorted_pagerank(dblp_2005), 50)
+# pr_dblp_2006 = get_first_N(get_sorted_pagerank(dblp_2006), 50)
+# pr_dblp_2005_w = get_first_N(get_sorted_pagerank(dblp_2005_w), 50)
+# print("Writing results ...")
+# write_json("pr_dblp_2005.json", pr_dblp_2005)
+# write_json("pr_dblp_2006.json", pr_dblp_2006)
+# write_json("pr_dblp_2005_w.json", pr_dblp_2005_w)
 
 ### Node and Edge Importance in Graphs ii) ###
-print("Getting Edge Betweeness importance ...")
-#Probably need top run with k=1000
-eb_dblp_2005 = get_first_N(get_sorted_edge_betweeness(dblp_2005, False), 20)
-eb_dblp_2006 = get_first_N(get_sorted_edge_betweeness(dblp_2006, False), 20)
-eb_dblp_2005_w = get_first_N(get_sorted_edge_betweeness(dblp_2005_w, True), 20)
-print("Writing results ...")
-write_json("eb_dblp_2005.json", eb_dblp_2005)
-write_json("eb_dblp_2006.json", eb_dblp_2006)
-write_json("eb_dblp_2005_w.json", eb_dblp_2005_w)
+# print("Getting Edge Betweeness importance ...")
+# #Probably need top run with k=1000
+# eb_dblp_2005 = get_first_N(get_sorted_edge_betweeness(dblp_2005, False), 20)
+# eb_dblp_2006 = get_first_N(get_sorted_edge_betweeness(dblp_2006, False), 20)
+# eb_dblp_2005_w = get_first_N(get_sorted_edge_betweeness(dblp_2005_w, True), 20)
+# print("Writing results ...")
+# write_json("eb_dblp_2005.json", eb_dblp_2005)
+# write_json("eb_dblp_2006.json", eb_dblp_2006)
+# write_json("eb_dblp_2005_w.json", eb_dblp_2005_w)
 
 ### Link Prediction in Graphs ###
 
@@ -133,35 +137,35 @@ print("Rpeoring ...")
 print(report_nodes_edges(dblp_2005_core))
 
 ## ii) ##
-print("Creating Graph, dblp_2006_core using nodes d >= 3 ...")
-dblp_2006_core = nx.Graph(dblp_2006)
-rm_2006 = [n for n,d in dict(dblp_2006_core.degree()).items() if d < 3]
-dblp_2006_core.remove_nodes_from(rm_2006)
-print("Reporting ...")
-print(report_nodes_edges(dblp_2006_core))
+# print("Creating Graph, dblp_2006_core using nodes d >= 3 ...")
+# dblp_2006_core = nx.Graph(dblp_2006)
+# rm_2006 = [n for n,d in dict(dblp_2006_core.degree()).items() if d < 3]
+# dblp_2006_core.remove_nodes_from(rm_2006)
+# print("Reporting ...")
+# print(report_nodes_edges(dblp_2006_core))
 
 ## iii) ###
-print("Generating Firends of Friends, 2 hops away  ...")
-s = dict(nx.all_pairs_shortest_path_length(dblp_2005_core, cutoff=2))
-fof = list()
-j = 0
-for key, value in s.items():
-    for k, v in value.items():
-        if v == 2:
-            fof.append({key, k})
-print(f"Friends of Friends: {len(fof)}")
+# print("Generating Firends of Friends, 2 hops away  ...")
+# s = dict(nx.all_pairs_shortest_path_length(dblp_2005_core, cutoff=2))
+# fof = list()
+# j = 0
+# for key, value in s.items():
+#     for k, v in value.items():
+#         if v == 2:
+#             fof.append({key, k})
+# print(f"Friends of Friends: {len(fof)}")
 
-## iv) ##
-print("Finidng difference of edges between dblp_2006_core and dblp_2005_core")
+# ## iv) ##
+# print("Finidng difference of edges between dblp_2006_core and dblp_2005_core")
 
-diff_dblp_2006_2005_core = nx.Graph(dblp_2006_core)
-diff_dblp_2006_2005_core.remove_edges_from(dblp_2005_core.edges)
-#this is the set of edges for the question
-t_edges = diff_dblp_2006_2005_core.edges
+# diff_dblp_2006_2005_core = nx.Graph(dblp_2006_core)
+# diff_dblp_2006_2005_core.remove_edges_from(dblp_2005_core.edges)
+# #this is the set of edges for the question
+# t_edges = diff_dblp_2006_2005_core.edges
 
-print(f"Number of edges in dblp_2005_core: {dblp_2005_core.number_of_edges()}")
-print(f"Number of edges in dblp_2006_core: {dblp_2006_core.number_of_edges()}")
-print(f"Number of edges in diff_dblp_2006_2005_core: {diff_dblp_2006_2005_core.number_of_edges()}")
+# print(f"Number of edges in dblp_2005_core: {dblp_2005_core.number_of_edges()}")
+# print(f"Number of edges in dblp_2006_core: {dblp_2006_core.number_of_edges()}")
+# print(f"Number of edges in diff_dblp_2006_2005_core: {diff_dblp_2006_2005_core.number_of_edges()}")
 
 ## v) ##
 
@@ -169,7 +173,24 @@ print(f"Number of edges in diff_dblp_2006_2005_core: {diff_dblp_2006_2005_core.n
 
 ### Community Detection in Graphs ###
 
+def community_detection(g):
+    k = 10
+    community_sizes = []
+    print("Applying Girvan-Newman detection ...")
+    com = girvan_newman(g)
+    limit = itertools.takewhile(lambda c: len(c) <= k, com)
 
+    for commun in limit:
+        for c in commun:
+            community_sizes.append(len(c))
+
+    return sorted(community_sizes, reverse=True)
+
+print("Creating smaller dblp_2005_core with d < 4")
+dblp_2005_core_small = nx.Graph(dblp_2005)
+rm_2005_small = [n for n,d in dict(dblp_2005_core_small.degree()).items() if d >= 4]
+dblp_2005_core_small.remove_nodes_from(rm_2005_small)
+print(community_detection(dblp_2005_core_small))
 
 
 
